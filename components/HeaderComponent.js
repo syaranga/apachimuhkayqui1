1a8096835d8c44145d26f6c36970d3d64445247b
jhon-04-alvitres <jhon.alvin1994@gmail.com>
2020-07-15 21:15:51 -0500

    HeaderCompnent created

---
+++ components/HeaderComponent.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+
+const styles = StyleSheet.create({
+  conatiner: {
+    height: hp('6%'),
+    justifyContent: 'center',
+    position: 'relative',
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray'
+  },
+  text: {
+    fontSize: hp('3%'),
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+  liveWrapper: {
+    height: hp('5%'),
+    width: hp('5%'),
+    borderRadius: wp('50%'),
+    backgroundColor: 'yellow',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: wp('5%'),
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderWidth: 2,
+    borderColor: 'yellow'
+  },
+  live: {
+    height: hp('4.5%'),
+    width: hp('4.5%'),
+    borderRadius: wp('50%'),
+    backgroundColor: 'green',
+    textAlign: 'center',
+    justifyContent: 'center'
+  },
+  liveText: {
+    color: 'white'
+  }
+})
+
+export class HeaderComponent extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <View style={[styles.conatiner, this.props.styleContainer]}>
+        <Text style={[styles.text, this.props.style]}>
+          {this.props.title}
+        </Text>
+        <View style={styles.liveWrapper} onPress={() => { }}>
+          <TouchableWithoutFeedback>
+            <View style={styles.live}>
+              <Text style={styles.liveText}>Live</Text>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default HeaderComponent
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import HeaderComponent from '../components/HeaderComponent'
 
 export class HomeScreen extends Component {
   render () {
     return (
       <View>
+        <HeaderComponent
+          title='apachimuchkayqui' style={{ textTransform: 'capitalize' }}
+          styleContainer={{
+            borderBottomWidth: 2,
+            borderBottomColor: 'rgba(0,0,0,.2)'
+          }}
+        />
         <Text>HomeScreen</Text>
       </View>
     )
