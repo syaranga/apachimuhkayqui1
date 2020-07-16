4d9653ad664ff08f60681a3a3358907c4be69e4d
Natalia <natalia21urbina@gmail.com>
2020-07-15 21:16:00 -0500

    HeaderComponent created

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import HeaderComponent from '../components/HeaderComponent'
 
 export class HomeScreen extends Component {
   render () {
     return (
       <View>
+        <HeaderComponent
+          title='apachimuhkayqui'
+          style={{ textTransform: 'capitalize' }}
+          styleContainer={{ borderBottonWidth: 2, borderBottonColor: 'rgba(0,0,0,.2)' }}
+        />
         <Text>HomeScreen</Text>
       </View>
     )
