9bdb6be033a7399e36e4eab567871bd1bd3b676c
napo <an7peru@gmail.com>
2020-07-16 14:01:09 -0500

    HeaderComponent create

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import HeaderComponent from '../components/HeaderComponent'
 
 export class CartScreen extends Component {
   render () {
     return (
       <View>
+        <HeaderComponent />
         <Text>CartScreen</Text>
       </View>
     )
