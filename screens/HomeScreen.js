5c4a6c2bb5689158e28846e919b7c56ceaacc715
user <user@kali>
2020-07-16 22:26:58 -0500

    SwiperComponent modified

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import { StatusBar, Text, ScrollView } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+
 import HeaderComponent from '../components/HeaderComponent'
 import SwiperComponent from '../components/SwiperComponent'
 
@@ -7,6 +9,7 @@ export class HomeScreen extends Component {
   render () {
     return (
       <ScrollView>
+        <StatusBar hidden />
         <HeaderComponent
           title='apachimuhkayqui'
           style={{ textTransform: 'capitalize' }}
@@ -15,7 +18,17 @@ export class HomeScreen extends Component {
             borderBottomColor: 'rgba(0,0,0,.2)'
           }}
         />
-        <SwiperComponent data={['a', 'b']} />
+        <SwiperComponent
+          data={[
+            { text: 'a', image: 'https://d13xymm0hzzbsd.cloudfront.net/1/20200710/15944262560050.png' },
+            { text: 'b', image: 'https://d13xymm0hzzbsd.cloudfront.net/1/20200710/15944262560866.png' },
+            { text: 'c', image: 'https://d13xymm0hzzbsd.cloudfront.net/1/20200714/15947777912309.png' }
+          ]}
+          styleContainer={{
+            height: hp('20%'),
+            backgroundColor: 'cyan'
+          }}
+        />
         <Text>HomeScreen</Text>
       </ScrollView>
     )
