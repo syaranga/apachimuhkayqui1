86b42e825e582a648b9e291b7cb8176c31d769b0
Hayde Yudissa Correa Campoverde <haydecorrea343@gmail.com>
2020-07-17 21:55:01 -0500

    SwiperComponent created

---
+++ Components/SwiperComponents.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import Swiper from 'react-native-swiper'
+
+const styles = StyleSheet.create({
+  container: { fix: 1, justifyContent: 'center', texAlign: 'center' }
+})
+export class SwiperComponent extends Component {
+  constructor (prosp) {
+    super(prosp)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <Swiper>
+        {
+          this.props.data.map((item, index) => {
+            return (
+
+              <View key={index} style={styles.container}>
+                <Text> {item} </Text>
+              </View>
+            )
+          })
+        }
+      </Swiper>
+    )
+  }
+}
+
+export default SwiperComponent
