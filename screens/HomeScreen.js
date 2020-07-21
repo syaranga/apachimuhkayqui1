a13d8751116f50e0929949845e0a262c5cd52f00
unknown <chaquilaperezmaria@gmail.com>
2020-07-20 23:08:30 -0500

    HomeScreen modified

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { StatusBar, Text, ScrollView } from 'react-native'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import SwiperComponent from '../components/SwiperComponent'
 import { TitleComponent } from '../components/TitleComponent'
+import { GalleryComponent } from '../components/GalleryComponent'
 
+export class HomeScreen extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar hidden />
+        <TitleComponent
+          title='Discover'
           styleContainer={{
             width: wp('95%'),
             marginHorizontal: wp('2.5%'),
             textShadowColor: 'gray',
-            textShadowOffset: {
+            textShadowOffeset: {
               width: hp('.3%'),
               height: hp('.3%')
             }
@@ -18,9 +27,18 @@ import { TitleComponent } from '../components/TitleComponent'
         />
         <SwiperComponent
           data={[
-            { text: 'a', image: 'https://agauche.org/wp-content/uploads/2020/03/alvan-nee-T-0EW-SEbsE-unsplash-scaled.jpg' },
-            { text: 'b', image: 'https://www.sosodours.com/wp-content/uploads//2019/07/Chien-plancher-odeur-urine.jpg' },
-            { text: 'c', image: 'https://www.igbioscience.co.uk/wp-content/uploads/2019/01/Colostrum-and-Pets-03-800x600.jpg' }
+            {
+              text: 'a',
+              image: 'https://agauche.org/wp-content/uploads/2020/03/alvan-nee-T-0EW-SEbsE-unsplash-scaled.jpg'
+            },
+            {
+              text: 'b',
+              image: 'https://www.sosodours.com/wp-content/uploads//2019/07/Chien-plancher-odeur-urine.jpg'
+            },
+            {
+              text: 'c',
+              image: 'https://www.igbioscience.co.uk/wp-content/uploads/2019/01/Colostrum-and-Pets-03-800x600.jpg'
+            }
           ]}
           styleContainer={{
             height: hp('50%'),
@@ -29,7 +47,7 @@ import { TitleComponent } from '../components/TitleComponent'
             backgroundColor: 'cyan'
           }}
         />
-          < TitleComponent/>
+        {/* <GalleryComponent /> */}
       </ScrollView>
     )
   }
