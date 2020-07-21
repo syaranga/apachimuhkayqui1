00dfd27ecd7e2a1edc6454b491caaf92a48fa150
Natalia <natalia21urbina@gmail.com>
2020-07-20 22:54:00 -0500

    save

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react'
-import { View, Text, ScrollView } from 'react-native'
-import HeaderComponent from '../components/HeaderComponent'
+import { StatusBar, Text, ScrollView } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+
 import SwiperComponent from '../components/SwiperComponent'
+import TitleComponent from '../components/TitleComponent'
 
 export class HomeScreen extends Component {
   render () {
     return (
       <ScrollView>
-        <HeaderComponent
-          title='apachimuhkayqui'
-          style={{ textTransform: 'capitalize' }}
+        <StatusBar hidden />
+        <TitleComponent
+          title='Discover'
           styleContainer={{
-            borderBottomWidth: 2,
-            borderBottomColor: 'rgba(0,0,0,.2)'
+            width: wp('95'),
+            marginHorizontal: wp('2.5%'),
+            textShadowColor: 'gray',
+            textShadowOffeset: {
+              width: hp('.3%'),
+              height: hp('.3%')
+            }
+          }}
+          style={{ fontSize: 36 }}
+
+        />
+
+        <SwiperComponent
+          data={[
+            { text: 'a', image: 'https://agauche.org/wp-content/uploads/2020/03/alvan-nee-T-0EW-SEbsE-unsplash-scaled.jpg' },
+            { text: 'b', image: 'https://www.sosodours.com/wp-content/uploads//2019/07/Chien-plancher-odeur-urine.jpg' },
+            { text: 'c', image: 'https://www.igbioscience.co.uk/wp-content/uploads/2019/01/Colostrum-and-Pets-03-800x600.jpg' }
+          ]}
+          styleContainer={{
+            height: hp('50%'),
+            width: wp('95%'),
+            marginHorizontal: wp('2.5%'),
+            backgroundColor: 'cyan'
           }}
         />
-        <SwiperComponent data={['a', 'b']} />
         <Text>HomeScreen</Text>
       </ScrollView>
+
     )
   }
 }
