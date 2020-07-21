4fb2adfbbd86ce9d5fb19cb1c80cca3cf9745e4b
jhon-04-alvitres <jhon.alvin1994@gmail.com>
2020-07-20 22:55:55 -0500

    HomeScreen modified

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,44 +1,53 @@
 import React, { Component } from 'react'
 import { StatusBar, Text, ScrollView } from 'react-native'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
-
-import HeaderComponent from '../components/HeaderComponent'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import SwiperComponent from '../components/SwiperComponent'
+import { TitleComponent } from '../components/TitleComponent'
+import { GalleryComponent } from '../components/GalleryComponent'
 
 export class HomeScreen extends Component {
   render () {
     return (
-      < ScrollView >
-      < StatusBar hidden/>
-        <HeaderComponent
-          title='apachimuchkayqui' 
-          style={{ textTransform: 'capitalize' }}
+      <ScrollView>
+        <StatusBar hidden />
+        <TitleComponent
+          title='Discover'
           styleContainer={{
-            borderBottomWidth: 2,
-            borderBottomColor: 'rgba(0,0,0,.2)'
+            width: wp('95%'),
+            marginHorizontal: wp('2.5%'),
+            textShadowColor: 'gray',
+            textShadowOffeset: {
+              width: hp('.3%'),
+              height: hp('.3%')
+            }
+          }}
+          style={{
+            fontSize: hp('8%')
           }}
         />
-        <SwiperComponent 
-        data={[
-          {
-            text: 'a',
-            image: 'https://d13xymm0hzzbsd.cloudfront.net/1/20200714/15947777912309.png'
-          },
-          {
-            text: 'b',
-            image: 'https://d13xymm0hzzbsd.cloudfront.net/1/20200710/15944262560866.png'
-          },
-          {
-            text: 'c',
-            image: 'https://d13xymm0hzzbsd.cloudfront.net/1/20200710/15944262560050.png'
-          }
-        ]}
-        styleContainer={{
-          height: hp('20%'),
-          backgroundColor: 'cyan'
-        }}
+        <SwiperComponent
+          data={[
+            {
+              text: 'a',
+              image: 'https://agauche.org/wp-content/uploads/2020/03/alvan-nee-T-0EW-SEbsE-unsplash-scaled.jpg'
+            },
+            {
+              text: 'b',
+              image: 'https://www.sosodours.com/wp-content/uploads//2019/07/Chien-plancher-odeur-urine.jpg'
+            },
+            {
+              text: 'c',
+              image: 'https://www.igbioscience.co.uk/wp-content/uploads/2019/01/Colostrum-and-Pets-03-800x600.jpg'
+            }
+          ]}
+          styleContainer={{
+            height: hp('50%'),
+            width: wp('95%'),
+            marginHorizontal: wp('2.5%'),
+            backgroundColor: 'cyan'
+          }}
         />
-        <Text>HomeScreen</Text>
+        <GalleryComponent />
       </ScrollView>
     )
   }
