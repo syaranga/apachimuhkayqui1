99a7b61b9731a81aa0cfdcf7924277021c7d1016
jhon-04-alvitres <jhon.alvin1994@gmail.com>
2020-07-21 20:18:16 -0500

    HomeScreen modified

--- components/SwiperComponent.js
+++ components/SwiperComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Image } from 'react-native'
 import Swiper from 'react-native-swiper/src'
 
 const styles = StyleSheet.create({
