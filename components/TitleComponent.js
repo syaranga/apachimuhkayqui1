b5c380287509588a1ffa76175ea1638c5f833042
jhon-04-alvitres <jhon.alvin1994@gmail.com>
2020-07-22 23:03:14 -0500

    GalleryCompnent

--- components/TitleComponent.js
+++ components/TitleComponent.js
@@ -5,13 +5,7 @@ export class TitleComponent extends Component {
   render () {
     return (
       <View style={this.props.styleContainer}>
-<<<<<<< HEAD
         <Text style={this.props.style}> {this.props.title} </Text>
-=======
-        <Text style={this.props.style}>
-          {this.props.title}
-        </Text>
->>>>>>> a12a6b9f89397e71543a4001aaa5cd5bde10bbf6
       </View>
     )
   }
