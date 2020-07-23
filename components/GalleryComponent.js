b1dfbbee971d258ce44035f68ee501942d74f807
jhon-04-alvitres <jhon.alvin1994@gmail.com>
2020-07-22 22:55:21 -0500

    GalleryCompnent

--- components/GalleryComponent.js
+++ components/GalleryComponent.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
-import { Text, View, ScrollView } from 'react-native'
+import { Text, View, ScrollView, Image, StyleSheet } from 'react-native'
 
 export class GalleryComponent extends Component {
   render () {
     return (
       <View style={[this.props.styleContainer]}>
-        <ScrollView>
+        <ScrollView horizontal style={Styles.ScrollView}>
           {
             this.props.data.map((item, index) => {
               return (
-                <View key={index}>
-                  <Text>
-                    {item.image}
-                    {item.avatar}
-                    {item.nickname}
-                    {item.alias}
-                  </Text>
+                <View style={Styles.item} key={index}>
+                  <View style={Styles.imageWraper} />
+                    <Image style={Styles.image} source={{ uri: item.image }} />
+                  </View>
+                   <View style={Styles.context} />
+                   <View style={Styles.user} />
+                  <Image style={Styles.avatar} source={{ uri: item.avatar }} />
+                  <View style={Styles.userData} />
+                  <Text style={Styles.nickname}> {item.nickname}</Text>
+                  <Text style={Styles.alias}> {item.alias}</Text>
+                   </View>
+                    </View>
+                  <Text style={Styles.price}> {item.price}</Text>
+                </View>
+                </View>
                 </View>
               )
             })
@@ -25,5 +33,14 @@ export class GalleryComponent extends Component {
     )
   }
 }
-
+const Styles = StyleSheet.create({
+  ScrollView: { height: '100%' },
+  item: { height: '100%', width: '95%', marginHorizontal: '2.5%' },
+  imageWraper: { height: '90%', width: '100%' },
+  image: { height: '90%', width: '100%' },
+  avatar: { height: '%', width: '%' },
+  nickname: {},
+  alias: {},
+  price: {}
+})
 export default GalleryComponent
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -24,18 +24,13 @@ export class HomeScreen extends Component {
             width: wp('95%'),
             marginHorizontal: wp('2.5%'),
             textShadowColor: 'gray',
-<<<<<<< HEAD
             textShadowOffeset: {
-=======
-            textShadowOffset: {
->>>>>>> a12a6b9f89397e71543a4001aaa5cd5bde10bbf6
               width: hp('.3%'),
               height: hp('.3%')
             }
           }}
           style={{
             fontSize: hp('8%')
-<<<<<<< HEAD
           }}
         />
         <SwiperComponent
@@ -60,24 +55,26 @@ export class HomeScreen extends Component {
             backgroundColor: 'cyan'
           }}
         />
-=======
+        <GalleryComponent data={[
+          {
+            image: 'https://images.unsplash.com/photo-1561229980-b674f5893cba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+          avatar: 'https://images.unsplash.com/photo-1561049841-d1d6a3e212fd?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=60',
+          nickname: 'ridhwam nord',
+          alias: '@ridzjcob',
+          price: '9876.54'
+        }
+        ]} 
+        styleContainer={{
+            height: hp('60%'),
+            width: wp('100%'),
+            backgroundColor: 'blue'
           }}
+          sytleItem={{
+             height: hp('50%'),
+              width: wp('95%'),
+              marginHorizontal: wp('2.5%')
+            }}
         />
-        <SwiperComponent
-          data={[
-            { text: 'a', image: 'https://agauche.org/wp-content/uploads/2020/03/alvan-nee-T-0EW-SEbsE-unsplash-scaled.jpg' },
-            { text: 'b', image: 'https://www.sosodours.com/wp-content/uploads//2019/07/Chien-plancher-odeur-urine.jpg' },
-            { text: 'c', image: 'https://www.igbioscience.co.uk/wp-content/uploads/2019/01/Colostrum-and-Pets-03-800x600.jpg' }
-          ]}
-          styleContainer={{
-            height: hp('50%'),
-            width: wp('95%'),
-            marginHorizontal: wp('2.5%'),
-            backgroundColor: 'cyan'
-          }}
-        />
->>>>>>> a12a6b9f89397e71543a4001aaa5cd5bde10bbf6
-        <GalleryComponent />
       </ScrollView>
     )
   }
