1da430b95c0196994c13a085eb47f8ce0da243c7
JoseEduardoSaavedraPichirri <eduardo.pichirri@outlook.com>
2020-07-22 20:59:26 -0500

    GalleyComponent created

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,55 +1,25 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
 import { StatusBar, Text, ScrollView } from 'react-native'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 import SwiperComponent from '../components/SwiperComponent'
 import TitleComponent from '../components/TitleComponent'
-=======
-import { StatusBar, ScrollView } from 'react-native'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
-import SwiperComponent from '../components/SwiperComponent'
-import TitleComponent from '../components/TitleComponent'
-import GalleryComponent from '../components/GalleryComponent'
->>>>>>> a12a6b9f89397e71543a4001aaa5cd5bde10bbf6
 
 export class HomeScreen extends Component {
   render () {
     return (
+
       <ScrollView>
         <StatusBar hidden />
         <TitleComponent
-<<<<<<< HEAD
-        title='Discover'
-        styleContainer={{
-        backgroundColor='red',
-        width: wp('95%'),
-        marginHorizontal=('2.5%')
-        }}
-        style={{
-          fontSize: 36
-        }}
-        />
-        <SwiperComponent
-          data={[
-            { text: 'a', image: 'https://agauche.org/wp-content/uploads/2020/03/alvan-nee-T-0EW-SEbsE-unsplash-scaled.jpg' },
-            { text: 'b', image: 'https://www.sosodours.com/wp-content/uploads//2019/07/Chien-plancher-odeur-urine.jpg' },
-            { text: 'c', image: 'https://www.igbioscience.co.uk/wp-content/uploads/2019/01/Colostrum-and-Pets-03-800x600.jpg' }
-          ]}
-          styleContainer={{
-=======
           title='Discover'
           styleContainer={{
+            backgroundColor: 'red',
             width: wp('95%'),
-            marginHorizontal: wp('2.5%'),
-            textShadowColor: 'gray',
-            textShadowOffset: {
-              width: hp('.3%'),
-              height: hp('.3%')
-            }
+            marginHorizontal: ('2.5%')
           }}
           style={{
-            fontSize: hp('8%')
+            fontSize: 36
           }}
         />
         <SwiperComponent
@@ -59,18 +29,13 @@ export class HomeScreen extends Component {
             { text: 'c', image: 'https://www.igbioscience.co.uk/wp-content/uploads/2019/01/Colostrum-and-Pets-03-800x600.jpg' }
           ]}
           styleContainer={{
->>>>>>> a12a6b9f89397e71543a4001aaa5cd5bde10bbf6
             height: hp('50%'),
             width: wp('95%'),
             marginHorizontal: wp('2.5%'),
             backgroundColor: 'cyan'
           }}
         />
-<<<<<<< HEAD
         <Text>HomeScreen</Text>
-=======
-        <GalleryComponent />
->>>>>>> a12a6b9f89397e71543a4001aaa5cd5bde10bbf6
       </ScrollView>
     )
   }
