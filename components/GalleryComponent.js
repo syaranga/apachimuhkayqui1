cd1936a6fab5723bf3f35e5d2875dfc4ad2140b1
medalitbu <medalitbancesubillus25@gmail.com>
2020-07-23 20:47:42 -0500

    GalleryComponent modified

--- components/GalleryComponent.js
+++ components/GalleryComponent.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Image } from 'react-native'
-import Gallery from 'react-native-swiper/src'
+import { Text, View, ScrollView, Image, StyleSheet } from 'react-native'
 
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', textAlign: 'center' }
-})
 export class GalleryComponent extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-
   render () {
     return (
       <View style={[this.props.styleContainer]}>
-        <Gallery>
-
+        <ScrollView horizontal style={styles.scrollView}>
           {
             this.props.data.map((item, index) => {
               return (
-                <View key={index} style={styles.container}>
-                  <Image
-                    style={{ height: '100%', width: '100%', resizeMode: 'cover' }}
-                    source={{ uri: item.image }}
-                  />
-                  <Text>
-                    <Text style={styles.text1}>PSimon Zhu</Text>
-                    <Text style={styles.text2}>@smnzhu</Text>
-                    <Text style={styles.text3}>$9876.54</Text>
-                  </Text>
+                <View style={[styles.item, this.props.styleItem]} key={index}>
+                  <View style={styles.imageWrapper}>
+                    <Image style={styles.image} source={{ uri: item.image }} />
+                  </View>
+                  <View style={styles.context}>
+                    <View style={styles.user}>
+                      <Image style={[styles.avatar, this.props.styleAvatar]} source={{ uri: item.avatar }} />
+                      <View style={styles.userData}>
+                        <Text style={styles.nickname}>{item.nickname}</Text>
+                        <Text style={styles.alias}>{item.alias}</Text>
+                      </View>
+                    </View>
+                    <View styles={styles.priceWrapper}>
+                      <Text style={styles.price}>{item.price}</Text>
+                    </View>
+                  </View>
                 </View>
               )
             })
           }
-        </Gallery>
+        </ScrollView>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  scrollView: {},
+  item: {},
+  imageWrapper: { height: '90%', width: '100%', marginBottom: '2%' },
+  image: { height: '100%', width: '100%' },
+  context: { flex: 1, flexDirection: 'row', justifyContent: 'space-between' },
+  user: { flex: 1, flexDirection: 'row' },
+  avatar: { marginRight: '5%' },
+  userData: {},
+  nickname: { fontSize: '120%' },
+  alias: {},
+  priceWrapper: {},
+  price: { fontSize: '200%' }
+})
 
 export default GalleryComponent
