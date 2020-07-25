065eac2dbb8f0e0775b4e6ee3237b33da99fe855
Alexandra Laura Benito <alexandralaurab7@gmail.com>
2020-07-24 21:47:38 -0500

    Save

--- components/SwiperComponent.js
+++ components/SwiperComponent.js
@@ -1,33 +1,4 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-import { Text, View, StyleSheet } from 'react-native'
-import Swiper from 'react-native-swiper/src'
-
-const styles = StyleSheet.create({
-    container: {fix: 1,justifyContent: 'center', textAlign: 'center'}
-})
-export class SwiperComponent extends Component {
-    constructor(props) {
-        super(props)
-        this.state= {}
-    }
-
-    render () {
-        return (
-         <Swiper>
-            {
-              this.props.data.map ((item, index)=> {
-                return (
-                    <View key={index} style= {styles.container}>
-                       <Text> {item} </Text>
-                    </View>
-                )
-              })
-            }            
-         </Swiper>
-        )
-    }
-=======
 import { Text, View, StyleSheet, Image } from 'react-native'
 import Swiper from 'react-native-swiper/src'
 
@@ -62,7 +33,6 @@ export class SwiperComponent extends Component {
       </View>
     )
   }
->>>>>>> 5c4a6c2bb5689158e28846e919b7c56ceaacc715
 }
 
 export default SwiperComponent
