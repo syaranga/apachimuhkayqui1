a2d82b47febcaca4ea22d996c58a30c69674d9a7
Jhener Jhony Acevedo Gonzales <jheneracevedo369@gmail.com>
2020-07-24 20:57:51 -0500

    GalleryComponent modified

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -35,10 +35,24 @@ export class HomeScreen extends Component {
             height: hp('50%'),
             width: wp('95%'),
             marginHorizontal: wp('2.5%'),
-            backgroundColor: 'cyan'
+            backgroundColor: '#F4F1F0'
           }}
         />
-        <GalleryComponent data={[]} />
+        <GalleryComponent
+          data={[
+            { image: 'https://images.unsplash.com/photo-1561435133-c44befe370ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60', avatar: 'https://images.unsplash.com/profile-1561381043166-4a4993b5f33d?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff', nickname: 'Ridhwan Nordin', alias: '@ridzjcob', precio: '$9876.54' },
+            { image: 'https://images.unsplash.com/photo-1561229980-b674f5893cba?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80', avatar: 'https://images.unsplash.com/profile-1537740391424-86188f82f007?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff', nickname: 'Clem Onojeghuo', alias: '@clemono2', precio: '$9876.54' },
+            { image: 'https://p1.piqsels.com/preview/572/646/384/man-standing-beside-motor-scooter-wall-paint.jpg', avatar: 'https://images.unsplash.com/profile-1528230399047-c8d0d832ed9d?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff', nickname: 'Jon Tyson', alias: '@jontyson', precio: '$9876.54' },
+            { image: 'https://images.unsplash.com/photo-1561049841-d1d6a3e212fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80', avatar: 'https://images.unsplash.com/profile-1536272917609-15221a21c8e6?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff', nickname: 'Simon Zhu', alias: '@smnzhu', precio: '$9876.54' }
+          ]}
+          styleContainer={{
+            marginVertical: wp('2.5%'),
+            height: hp('70%'),
+            width: wp('95%'),
+            marginHorizontal: wp('2.5%'),
+            backgroundColor: '#F4F1F0'
+          }}
+        />
       </ScrollView>
     )
   }
