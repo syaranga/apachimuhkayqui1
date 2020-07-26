2e8d2d58a8c05da4e0ec3a26d0cc37a5cbeacdd9
marinreyes <marinreyes1998@gmail.com>
2020-07-26 18:11:22 -0500

    save copy

---
+++ components/GridComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class GridComponent extends Component {
+  render () {
+    return (
+      <View>
+        <Text> GridComponent </Text>
+      </View>
+    )
+  }
+}
+export default GridComponent
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import TitleComponent from '../components/TitleComponent'
+import GridComponent from '../components/GridComponent'
 
 export class SearchScreen extends Component {
   render () {
     return (
       <View>
+        <TitleComponent
+          title='Search'
+          styleContainer={{
+            width: wp('95%'),
+            marginHorizontal: wp('2.5%'),
+            textShadowColor: 'gray',
+            textShadowOffset: {
+              width: hp('.3%'),
+              height: hp('.3%')
+            }
+          }}
+          style={{
+            fontSize: hp('8%')
+          }}
+        />
+        <GridComponent />
         <Text>SearchScreen</Text>
       </View>
     )
