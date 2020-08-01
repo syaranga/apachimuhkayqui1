8791ff2d863eb1b614860b3241a217f7efd4e9b4
Johan Brandon Arapa Damiano <johanx369@gmail.com>
2020-07-31 20:57:17 -0500

    GridComponent

--- components/GridComponent.js
+++ components/GridComponent.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, StyleSheet, TextInput } from 'react-native'
 import { FlatGrid } from 'react-native-super-grid'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 export class GridComponent extends Component {
   render () {
     return (
-      <View>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+        />
+        <Text style={styles.allRes}>ALL RESULTS</Text>
+        <Text style={styles.pag}> {'(<)1 de 100(>)'} </Text>
         <FlatGrid
           itemDimension={this.props.itemDimension}
           data={this.props.data}
           renderItem={({ item }) => (
             <View style={this.props.styleItem}>
-              <Image
-                style={{ resizeMode: 'cover' }}
-                source={{ uri: item.image }}
-              />
-              <Text>{item.discount}</Text>
-              <Text>{item.price}</Text>
+              <Image source={{ uri: item.image }} style={{ width: wp('30%'), height: hp('19%') }} />
+              <View style={styles.textWrapper}>
+                <Text style={styles.discount}>{item.discount}</Text>
+                <Text style={styles.price}>{item.price}</Text>
+              </View>
             </View>
           )}
         />
+        <Text style={styles.pagFinal}> {'(<)1 de 100(>)'} </Text>
       </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  container: { backgroundColor: 'white', paddingBottom: wp('5%') },
+  input: { borderWidth: 2, padding: wp('2.5%'), marginHorizontal: wp('2.5%') },
+  allRes: { marginHorizontal: wp('2.5%'), fontWeight: 1000, fontSize: 12, marginTop: hp('1%') },
+  pag: { fontFamily: 'Arial', fontSize: 13, textAlign: 'center', position: 'relative', top: 8, fontWeight: 510 },
+  pagFinal: { fontFamily: 'Arial', fontSize: 13, textAlign: 'center', position: 'relative', bottom: 13, fontWeight: 510 },
+  textWrapper: { backgroundColor: '#FFFFFF50', flexDirection: 'row', marginBottom: hp('0.5%'), position: 'absolute', width: wp('30%') },
+  discount: { color: 'red', fontWeight: 550, fontSize: 14, marginLeft: 2, fontFamily: 'Century Gothic' },
+  price: { fontSize: 14, position: 'relative', marginLeft: wp('9.5%'), fontWeight: 549.5, fontFamily: 'Century Gothic' }
+})
 export default GridComponent
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -26,23 +26,160 @@ export class SearchScreen extends Component {
         <GridComponent
           data={[
             {
-              image: 'https://mister-mango.omni.la/ProductCatalog/Workspace.CYC3VPWVK2T2U/ProductCatalog.CYSBZKMVL7GKW/1500x1500/CYSC4CM44J2AQ.jpg',
-              discount: '0%',
-              price: '8.5'
+              image: 'https://www.terranovacnc.com/wp-content/uploads/2018/10/jonatan-burneo-788265-unsplash-scaled.jpg',
+              discount: '70%',
+              price: '$999.9'
             },
             {
-              image: 'https://wowpooch.com/wp-content/uploads/2019/05/Best-Dog-Food-For-Giant-Breeds-1024x536.jpg',
-              discount: '0%',
-              price: '8.5'
+              image: 'https://www.insidehook.com/wp-content/uploads/2020/03/dog_adoptions_coronavirus.jpg?fit=4000%2C2674',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://64.media.tumblr.com/e37f98b27aab088f3951ef1f62835796/00eba40805112bfd-db/s1280x1920/a9a92f1bbf0402757cfc95860d6eb76703828c52.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://www.adiestrar-perros.com/blog/wp-content/uploads/2019/10/Two-dogs-1080x675.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://canna-pet.com/wp-content/uploads/2019/05/jenny-marvin-1265841-unsplash.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://cdn.shopify.com/s/files/1/0248/8729/6077/articles/1_bbGSypCPuFoVzncKdcElfg_2670x.jpeg?v=1584767393',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://st.focusedcollection.com/9163412/i/650/focused_176669450-stock-photo-brown-labrador-dog-posing-shore.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://miro.medium.com/max/7214/0*MLWCIROxXBu7i12n',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://i1.wp.com/glamorouspuppies.com/wp-content/uploads/2019/05/GlamPup_Post_Guide_1.jpg?fit=857%2C1200&ssl=1',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://cdn.hk01.com/di/media/images/3036477/org/248e7f27fa9b7684e042fc45e7371887.jpg/p2yyASHpWN-Fu3AE_rn38zj3IyBZlU_LQLtO6kC7Tuo',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://miro.medium.com/max/7744/1*-w_DAw9x8lNyIkbnvv1NPw.jpeg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://capuybigotes.com/wp-content/uploads/2020/04/Imagen-destacada_Mesa-de-trabajo-1-16.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://revistaminha.pt/wp-content/uploads/2019/09/anoir-chafik-2_3c4dIFYFU-unsplash-1080x700.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://skidawaytimes.com/wp-content/uploads/2018/04/20637789_1700677909976225_2179060765362220686_n.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://www.mundosano.com/__export/1593142131163/sites/elimparcial/img/2020/06/25/dogs-1246588_1920.jpg_423682103.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://s3-ap-northeast-1.amazonaws.com/static.lovely-media.jp/production/imgs/images/000/112/968/original.?1527717463',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://wamiz-international-prod.azureedge.net/media/cache/upload_og-image/article/main-picture/5c3d9c8052935873909725.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://miro.medium.com/max/9216/0*vjGEeATG3dK63iJ3',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://magarticles.magzter.com/articles/10496/415296/5e4dde9fab191/Love-Unconditionally.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://www.happets.com/blog/wp-content/uploads/2020/04/jay-wennington-CdK2eYhWfQ0-unsplash-1-1-1-1180x787.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://images-na.ssl-images-amazon.com/images/I/81wMB2xwkdL.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://images.squarespace-cdn.com/content/582a403fe58c62e99d839398/1561407089126-AJVN2HUV0TF0NEMYY9AQ/image-asset.jpeg?format=1500w&content-type=image%2Fjpeg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://mliq8vbttm5n.i.optimole.com/f5wiKvI-3_-5yxWi/w:auto/h:auto/q:75/https://blundellcashmere.com/wp-content/uploads/2019/01/Diferenciar-cashmere-de-calidad-Blundell-Blog-e1551262153912.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://i.pinimg.com/originals/6d/c3/04/6dc3041c2fc87e2106cb76b7d188181a.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://st.focusedcollection.com/9163412/i/650/focused_176669450-stock-photo-brown-labrador-dog-posing-shore.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://miro.medium.com/max/7214/0*MLWCIROxXBu7i12n',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://i1.wp.com/glamorouspuppies.com/wp-content/uploads/2019/05/GlamPup_Post_Guide_1.jpg?fit=857%2C1200&ssl=1',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://www.adiestrar-perros.com/blog/wp-content/uploads/2019/10/Two-dogs-1080x675.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://canna-pet.com/wp-content/uploads/2019/05/jenny-marvin-1265841-unsplash.jpg',
+              discount: '70%',
+              price: '$999.9'
+            },
+            {
+              image: 'https://cdn.shopify.com/s/files/1/0248/8729/6077/articles/1_bbGSypCPuFoVzncKdcElfg_2670x.jpeg?v=1584767393',
+              discount: '70%',
+              price: '$999.9'
             }
           ]}
-          itemDimension={wp('30%')}
+          itemDimension={wp('25%')}
           styleItem={{
             height: wp('30%'),
-            justifyContent: 'flex-end',
-            borderRadius: wp('2%'),
-            padding: wp('.5%'),
-            backgroundColor: 'red'
+            justifyContent: 'flex-end'
           }}
         />
       </ScrollView>
