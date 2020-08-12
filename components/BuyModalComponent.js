c129c2d3e6ed8ea046db2248084cf6b79fea783b
Edgar Michael Vargas Yanque <michaelvargas0301@gmail.com>
2020-08-11 21:20:59 -0500

    resaved

--- components/BuyModalComponent.js
+++ components/BuyModalComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import Modal from 'react-native-modal'
+import { Text, View, Modal, StyleSheet } from 'react-native'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import TitleComponent from './TitleComponent'
 import GalleryComponent from './GalleryComponent'
@@ -9,119 +8,122 @@ import { Entypo } from '@expo/vector-icons'
 export class BuyModalComponent extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      isVisible: false
-    }
-  }
-
-  componentDidMount () {
-    this.setState({ isVisible: this.props.visible })
-  }
-
-  componentDidUpdate (prevProps) {
-    if (this.props.visible !== prevProps.visible) {
-      this.setState({ isVisible: this.props.visible })
-    }
+    this.state = {}
   }
 
   render () {
-    if (this.state.isVisible) {
-      return (
-        <View style={{ position: 'absolute', zIndex: 50, elevation: 50 }}>
-          <Modal
-            isVisible={this.state.isVisible}
-            style={{ backgroundColor: 'white', width: wp('90%'), height: hp('80%'), marginHorizontal: wp('5%'), borderRadius: wp('5%') }}
-            onBackdropPress={() => this.setState({ isVisible: false })}
-          >
-            <View style={{ flex: 1 }}>
-              <TitleComponent
-                title='Buy'
-                styleContainer={{
-                  width: wp('95%'),
-                  marginHorizontal: wp('2.5%'),
-                  textShadowColor: 'gray',
-                  textShadowOffset: {
-                    width: hp('.3%'),
-                    height: hp('.3%')
-                  }
-                }}
-                style={{
-                  fontSize: hp('8%')
-                }}
-              />
-              <Entypo
-                name='heart' size={30} color='#F011A2'
-                style={styles.entypoIcon}
-              />
-              <GalleryComponent
-                data={[
-                  {
-                    image: 'https://www.barkyn.com/blog/img/uploads/2019/09/jonatan-burneo-NTyH57Qqiu8-unsplash-1.jpg',
-                    avatar: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg',
-                    nickname: 'Ridhwan nordin',
-                    alias: '@ridzjcob',
-                    price: '9876.54'
-                  },
-                  {
-                    image: 'https://www.insidehook.com/wp-content/uploads/2020/03/dog_adoptions_coronavirus.jpg?fit=1795%2C1200',
-                    avatar: 'https://manofmany.com/wp-content/uploads/2019/06/50-Long-Haircuts-Hairstyle-Tips-for-Men-2.jpg',
-                    nickname: 'Clem onojeghuo',
-                    alias: '@clemono2',
-                    price: '9876.54'
-                  },
-                  {
-                    image: 'https://th.bing.com/th/id/OIP.roXztmu92HGfZmh-T6x4awHaHa?pid=Api&w=203&h=203&c=7',
-                    avatar: 'https://newcastlebeach.org/images/man-picture.jpg',
-                    nickname: 'Jon tyson',
-                    alias: '@jontyson',
-                    price: '9876.54'
-                  },
-                  {
-                    image: 'https://www.bing.com/images/blob?bcid=TtybFVvDHJsBsw',
-                    avatar: 'https://i.pinimg.com/originals/cb/78/2b/cb782bd9da27e7b6dcd7974d2bb3a42e.jpg',
-                    nickname: 'Simon zhu',
-                    alias: '@smnzhu',
-                    price: '9876.54'
-                  }
-                ]}
-                styleContainer={{
-                  height: hp('55%'),
-                  width: wp('95%'),
-                  marginBottom: wp('2.5%')
-                }}
-                styleItem={{
-                  height: hp('50%'),
-                  width: wp('90%'),
-                  marginHorizontal: wp('2.5%')
-                }}
-                styleAvatar={{
-                  height: hp('7%'),
-                  width: hp('7%'),
-                  borderRadius: hp('7%')
-                }}
-                stylePrice={{
-                  fontSize: hp('5%')
-                }}
-                styleNickname={{
-                  fontSize: hp('3%'),
-                  fontWeight: 'bold'
-                }}
-                styleAlias={{
-                  fontSize: hp('2%')
-                }}
-              />
-              <View style={styles.addButton}>
-                <Text style={styles.addText}>Add to Cart</Text>
-              </View>
-            </View>
-          </Modal>
-        </View>
-      )
-    }
-    return null
+    return (
+      <View style={styles.modalContainer}>
+        <Modal
+          visible={this.props.visible}
+          style={styles.modalStyle}
+        >
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <TitleComponent
+              title='Buy'
+              styleContainer={{
+                width: wp('95%'),
+                marginHorizontal: wp('2.5%'),
+                textShadowColor: 'gray',
+                textShadowOffset: {
+                  width: hp('.3%'),
+                  height: hp('.3%')
+                }
+              }}
+              style={{
+                fontSize: hp('7%')
+              }}
+            />
+            <Entypo
+              name='heart' size={30} color='#F011A2'
+              style={styles.entypoIcon}
+            />
+          </View>
+
+          <GalleryComponent
+            data={[
+              {
+                image: 'https://www.barkyn.com/blog/img/uploads/2019/09/jonatan-burneo-NTyH57Qqiu8-unsplash-1.jpg',
+                avatar: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg',
+                nickname: 'Ridhwan nordin',
+                alias: '@ridzjcob',
+                price: '9876.54'
+              },
+              {
+                image: 'https://www.insidehook.com/wp-content/uploads/2020/03/dog_adoptions_coronavirus.jpg?fit=1795%2C1200',
+                avatar: 'https://manofmany.com/wp-content/uploads/2019/06/50-Long-Haircuts-Hairstyle-Tips-for-Men-2.jpg',
+                nickname: 'Clem onojeghuo',
+                alias: '@clemono2',
+                price: '9876.54'
+              },
+              {
+                image: 'https://th.bing.com/th/id/OIP.roXztmu92HGfZmh-T6x4awHaHa?pid=Api&w=203&h=203&c=7',
+                avatar: 'https://newcastlebeach.org/images/man-picture.jpg',
+                nickname: 'Jon tyson',
+                alias: '@jontyson',
+                price: '9876.54'
+              },
+              {
+                image: 'https://www.bing.com/images/blob?bcid=TtybFVvDHJsBsw',
+                avatar: 'https://i.pinimg.com/originals/cb/78/2b/cb782bd9da27e7b6dcd7974d2bb3a42e.jpg',
+                nickname: 'Simon zhu',
+                alias: '@smnzhu',
+                price: '9876.54'
+              }
+            ]}
+            styleContainer={{
+              height: hp('55%'),
+              width: wp('95%'),
+              marginBottom: wp('2.5%')
+            }}
+            styleItem={{
+              height: hp('50%'),
+              width: wp('90%'),
+              marginHorizontal: wp('2.5%')
+            }}
+            styleAvatar={{
+              height: hp('7%'),
+              width: hp('7%'),
+              borderRadius: hp('7%')
+            }}
+            stylePrice={{
+              fontSize: hp('5%')
+            }}
+            styleNickname={{
+              fontSize: hp('3%'),
+              fontWeight: 'bold'
+            }}
+            styleAlias={{
+              fontSize: hp('2%')
+            }}
+          />
+          <View style={styles.addButton}>
+            <Text style={styles.addText}>Add to Cart</Text>
+          </View>
+        </Modal>
+      </View>
+    )
   }
 }
+
 const styles = StyleSheet.create({
+  modalContainer: {
+    width: wp('100%'),
+    height: hp('100%'),
+    zIndex: 50,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: 'rgba(0,0,0,.5)'
+  },
+  modalStyle: {
+    width: wp('95%'),
+    height: hp('77.5%'),
+    marginHorizontal: wp('2.5%'),
+    borderRadius: wp('3%'),
+    backgroundColor: 'white',
+    paddingBottom: hp('2%')
+  },
   entypoIcon: {
     position: 'absolute',
     right: 0,
@@ -139,5 +141,4 @@ const styles = StyleSheet.create({
     fontWeight: '600'
   }
 })
-
 export default BuyModalComponent
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -5,17 +5,29 @@ import TitleComponent from '../components/TitleComponent'
 import GridComponent from '../components/GridComponent'
 import BuyModalComponent from '../components/BuyModalComponent'
 
-export class SearchScreen extends Component {
+class SearchScreen extends Component {
+  constructor () {
+    super()
+
+    this.navigate = this.navigate.bind(this)
+  }
+
+  navigate (name) {
+    this.props.navigator.push({
+      name
+    })
+  }
+
   render () {
     return (
       <View>
+        <BuyModalComponent visible />
         <ScrollView>
-          <BuyModalComponent visible />
           <TitleComponent
             title='Search'
             styleContainer={{
-              width: wp('95%'),
-              marginHorizontal: wp('2.5%'),
+              width: wp('100%'),
+              paddingHorizontal: wp('2.5%'),
               textShadowColor: 'gray',
               textShadowOffset: {
                 width: hp('.3%'),
@@ -23,104 +35,122 @@ export class SearchScreen extends Component {
               }
             }}
             style={{
-              fontSize: hp('8%')
+              fontSize: hp('7%')
             }}
           />
           <GridComponent
             handleOnPress={(item) => {
-              console.log('pressed!')
+              console.log('press')
               console.log(item)
             }}
             data={[
               {
                 image: 'https://www.barkyn.com/blog/img/uploads/2019/09/jonatan-burneo-NTyH57Qqiu8-unsplash-1.jpg',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 1
               },
               {
                 image: 'https://www.insidehook.com/wp-content/uploads/2020/03/dog_adoptions_coronavirus.jpg?fit=1795%2C1200',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 2
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.roXztmu92HGfZmh-T6x4awHaHa?pid=Api&w=203&h=203&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 3
               },
               {
                 image: 'https://www.bing.com/images/blob?bcid=TtybFVvDHJsBsw',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 4
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.Trws725tqh9vm5LNukOOrwHaE5?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 5
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.dDMUv2x_HRcYmfXRjHusGQAAAA?pid=Api&w=120&h=86&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 6
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.02jiiV5WLMpG5BfavUvXUQAAAA?pid=Api&w=100&h=70&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 7
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.K778MHeRxj6tkhi7bVZHtwHaE7?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 8
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.zFngqtjyu0nHsZooU_MmZwAAAA?pid=Api&w=100&h=100&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 9
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.Rh1abxgSzyV-U32eBTr4HgAAAA?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 10
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.fN8lBBz-56V3jOMap2hZrAAAAA?pid=Api&w=90&h=90&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 11
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.m_QjqiyuLb9aAKeachidUAHaHa?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 12
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.02jiiV5WLMpG5BfavUvXUQAAAA?pid=Api&w=100&h=70&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 13
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.K778MHeRxj6tkhi7bVZHtwHaE7?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 14
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.zFngqtjyu0nHsZooU_MmZwAAAA?pid=Api&w=100&h=100&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 15
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.Rh1abxgSzyV-U32eBTr4HgAAAA?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 16
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.fN8lBBz-56V3jOMap2hZrAAAAA?pid=Api&w=90&h=90&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 17
               },
               {
                 image: 'https://th.bing.com/th/id/OIP.m_QjqiyuLb9aAKeachidUAHaHa?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
-                price: '$9999'
+                price: '$9999',
+                id: 18
               }
             ]}
             itemDimension={wp('25%')}
