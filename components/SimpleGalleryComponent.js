c5f3ba7f88beaf114ca1b4ee4e163a812be746b7
Binni Zenobio Cordova Leandro <binni.2000.cordova@gmail.com>
2020-11-16 20:57:12 -0500

    woring on SimpleGalleryComponent.js

---
+++ components/SimpleGalleryComponent.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
+
+export default class SimpleGalleryComponent extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.slide}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          {
+            (this.props.data && this.props.data.length >= 1) && (
+              this.props.data.map((item, index) => {
+                return (
+                  <View key={index} style={styles.item}>
+                    <Image style={styles.image} source={{ uri: item.image }} />
+                  </View>
+                )
+              })
+            )
+          }
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  slide: {},
+  item: {},
+  image: { width: 100, height: 100, marginHorizontal: 5 }
+})
