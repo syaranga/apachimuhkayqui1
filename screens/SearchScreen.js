a139e5a419643b41f82b88b10e2f2cad8f02d086
Binni Zenobio Cordova Leandro <binni.2000.cordova@gmail.com>
2020-11-21 19:58:35 -0500

    working on SearchScreen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
-import { ScrollView, View } from 'react-native'
+import { ScrollView, TouchableWithoutFeedbackBase, View } from 'react-native'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import TitleComponent from '../components/TitleComponent'
 import GridComponent from '../components/GridComponent'
 import BuyModalComponent from '../components/BuyModalComponent'
 
 export class SearchScreen extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query: '{ Items { id, user_id, brand, model, description, price, image, user {id, fullname, alias, email, phone, document}}}'
+      })
+    })
+      .then((result) => result.json())
+      .then(({ data }) => this.setState({ items: data }))
+      .catch((err) => console.error(err))
+  }
+
   render () {
     return (
       <View>
@@ -20,91 +43,6 @@ export class SearchScreen extends Component {
             data={[
               {
                 image: 'https://www.barkyn.com/blog/img/uploads/2019/09/jonatan-burneo-NTyH57Qqiu8-unsplash-1.jpg',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://www.insidehook.com/wp-content/uploads/2020/03/dog_adoptions_coronavirus.jpg?fit=1795%2C1200',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.roXztmu92HGfZmh-T6x4awHaHa?pid=Api&w=203&h=203&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://www.bing.com/images/blob?bcid=TtybFVvDHJsBsw',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.Trws725tqh9vm5LNukOOrwHaE5?pid=Api&w=84&h=84&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.dDMUv2x_HRcYmfXRjHusGQAAAA?pid=Api&w=120&h=86&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.02jiiV5WLMpG5BfavUvXUQAAAA?pid=Api&w=100&h=70&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.K778MHeRxj6tkhi7bVZHtwHaE7?pid=Api&w=84&h=84&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.zFngqtjyu0nHsZooU_MmZwAAAA?pid=Api&w=100&h=100&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.Rh1abxgSzyV-U32eBTr4HgAAAA?pid=Api&w=84&h=84&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.fN8lBBz-56V3jOMap2hZrAAAAA?pid=Api&w=90&h=90&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.m_QjqiyuLb9aAKeachidUAHaHa?pid=Api&w=84&h=84&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.02jiiV5WLMpG5BfavUvXUQAAAA?pid=Api&w=100&h=70&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.K778MHeRxj6tkhi7bVZHtwHaE7?pid=Api&w=84&h=84&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.zFngqtjyu0nHsZooU_MmZwAAAA?pid=Api&w=100&h=100&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.Rh1abxgSzyV-U32eBTr4HgAAAA?pid=Api&w=84&h=84&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.fN8lBBz-56V3jOMap2hZrAAAAA?pid=Api&w=90&h=90&c=7',
-                discount: '70%',
-                price: '$9999'
-              },
-              {
-                image: 'https://th.bing.com/th/id/OIP.m_QjqiyuLb9aAKeachidUAHaHa?pid=Api&w=84&h=84&c=7',
                 discount: '70%',
                 price: '$9999'
               }
