e26f06ed8bcd8993c4aad72cdc8b48f6901b28a7
Saul Ventura <venturagalvez1998@gmail.con>
2020-12-12 17:37:48 -0500

    local changes

--- components/SlideItemComponent.js
+++ components/SlideItemComponent.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
 
-export class SlideItemComponent extends Component {
+export default class SlideItemComponent extends Component {
   render () {
     return (
       <View>
-        <Text> SlideItemComponent </Text>
+        <ScrollView>
+          <SlideItemComponent
+            data={this.props.data}
+          />
+        </ScrollView>
       </View>
     )
   }
 }
 
-export default SlideItemComponent
+export class SlideComponent extends Component {
+  render () {
+    return (
+      <View>
+        {
+          (this.props.data && this.props.data.length >= 1) && (
+            this.props.data.map((item, index) => {
+              return (
+                <ItemComponent data={item} key={index} />
+              )
+            })
+          )
+        }
+      </View>
+    )
+  }
+}
+
+export class ItemComponent extends Component {
+  render () {
+    return (
+      <View style={{ height: 200, width: 100 }}>
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: this.props.data.image }}
+        />
+        <Text>{this.props.data.brand}</Text>
+      </View>
+    )
+  }
+}
