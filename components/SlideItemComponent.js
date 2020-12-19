a722151f93256bf3286b32927efd0d0d692891aa
Saul Ventura <venturagalvez1998@gmail.con>
2020-12-19 17:05:48 -0500

    local changes

--- components/SlideItemComponent.js
+++ components/SlideItemComponent.js
@@ -31,7 +31,34 @@ export class ItemComponent extends Component {
           style={{ flex: 1 }}
           source={{ uri: this.props.data.image }}
         />
-        <Text>{this.props.data.brand}</Text>
+        <ForegroundItemComponent {...this.props} />
+      </View>
+    )
+  }
+}
+
+export class ForegroundItemComponent extends Component {
+  render () {
+    return (
+      <View style={styles.foreground}>
+        <View style={{ backgroundColor: 'red' }}>
+          <View style={[styles.row, { backgroundColor: 'green' }]}>
+            <Text>{this.props.data.brand}</Text>
+            <Text>{this.props.data.original_price}</Text>
+          </View>
+          <View style={[styles.row, { backgroundColor: 'blue' }]}>
+            <Text>{this.props.data.model}</Text>
+            <Text>{this.props.data.price}</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>add</Text>
+          </View>
+          <View>
+            <Text>love</Text>
+          </View>
+        </View>
       </View>
     )
   }
@@ -42,8 +69,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: wp('2.5%')
   },
   item: {
-    width: wp('30%'),
-    height: wp('45%'),
+    position: 'relative',
+    width: wp('46.25%'),
+    height: wp('46.25%') * 1.32,
     marginRight: wp('2.5%')
+  },
+  foreground: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(200,200,20,.5)'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'space-between'
   }
 })
