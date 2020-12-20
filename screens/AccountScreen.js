10f947f09d6554b73c3ed9f3dfc629235bca2d0c
Joseph-CA <josephtex1066@gmail.com>
2020-12-19 19:21:06 -0500

    19/12/20

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -35,19 +35,33 @@ export default class AccountScreen extends Component {
       }
     }
 
-    fetch('http://localhost:4000/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
+    this.handleOnFetch(
+      'http://localhost:4000/graphql',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          query,
+          variables
+        })
       },
-      body: JSON.stringify({
-        query,
-        variables
-      })
-    })
+      'updateUser'
+    )
+  }
+
+  handleOnFetch (input, init, control, object) {
+    fetch(input, init)
       .then(r => r.json())
-      .then(data => console.log('data returned:', data))
+      .then(result => {
+        if (control === 'updateUser') {
+          console.log('user:', result.data)
+          this.setState({ user: result.data })
+        }
+      })
+
       .catch((err) => console.error(err))
   }
 
