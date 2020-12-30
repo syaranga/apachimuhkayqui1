514ff4352dad620dead41a31e33566f9edb17483
Saul Ventura <venturagalvez1998@gmail.con>
2020-12-29 20:08:22 -0500

    SlideItemComponent.js

---
+++ components/ButtonIconComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen'
+
+export default class ButtonComponent extends Component {
+  render () {
+    return (
+      <View style={[styles.container, this.props.styleContainer]}>
+        <AntDesign
+          style={[styles.icon, this.props.styleIcon]}
+          name={this.props.name}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: wp('8%'),
+    height: wp('8%'),
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  icon: {
+    color: '#000000',
+    fontSize: 24,
+    textAlign: 'center'
+  }
+})
--- components/SlideItemComponent.js
+++ components/SlideItemComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Image, ScrollView, StyleSheet, Text, View, VirtualizedList } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import ButtonIconComponent from '../components/ButtonIconComponent'
 
 export default class SlideItemComponent extends Component {
   render () {
@@ -41,22 +42,24 @@ export class ForegroundItemComponent extends Component {
   render () {
     return (
       <View style={styles.foreground}>
-        <View style={{ backgroundColor: 'red' }}>
-          <View style={[styles.row, { backgroundColor: 'green' }]}>
-            <Text>{this.props.data.brand}</Text>
-            <Text>{this.props.data.original_price}</Text>
+        <View style={{ paddingVertical: wp('2.5%'), backgroundColor: 'rgba(255,255,255,0.5)' }}>
+          <View style={[styles.rowWrapper]}>
+            <View style={[styles.row, { flex: 0.9 }]}>
+              <Text>{this.props.data.brand}</Text>
+              <Text>{this.props.data.original_price}</Text>
+            </View>
           </View>
-          <View style={[styles.row, { backgroundColor: 'blue' }]}>
-            <Text>{this.props.data.model}</Text>
-            <Text>{this.props.data.price}</Text>
+          <View style={[styles.rowWrapper]}>
+            <View style={[styles.row, { flex: 0.9 }]}>
+              <Text>{this.props.data.model}</Text>
+              <Text>{this.props.data.price}</Text>
+            </View>
           </View>
         </View>
-        <View>
-          <View>
-            <Text>add</Text>
-          </View>
-          <View>
-            <Text>love</Text>
+        <View style={[styles.rowWrapper, { paddingVertical: wp('2.5%') }]}>
+          <View style={[styles.row, { flex: 0.9 }]}>
+            <ButtonIconComponent name='shoppingcart' />
+            <ButtonIconComponent name='hearto' />
           </View>
         </View>
       </View>
@@ -80,11 +83,17 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     bottom: 0,
-    backgroundColor: 'rgba(200,200,20,.5)'
+    justifyContent: 'flex-end',
+    alignContent: 'flex-end'
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignContent: 'space-between'
+  },
+  rowWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center'
   }
 })
